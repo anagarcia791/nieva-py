ce107eae70a1f89493046303a0edcffb0a027f2d
Ana Garcia <anamargarita.garciat@slalom.com>
2025-11-15 15:48:42 -0500

    week_4

---
+++ week_4.py
@@ -0,0 +1,14 @@
+# region Functions ------------------------c
+'''Ejercicio 1
+Crea una función bienvenida() que imprima “¡Hola, estudiante!”.'''
+def welcome():
+    print("¡Hi, student!")
+welcome()
+
+'''Ejercicio 2
+Crea una función presentacion(nombre) que reciba un nombre y muestre un saludo personalizado.'''
+def presentation(name):
+    print(f"¡Hi, {name}!")
+presentation("Ana")
+
+# endregionv
